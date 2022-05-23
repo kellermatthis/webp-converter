7e8311b5e6662b301b12048df8d4ebca4400fd56
kellermatthis <github@matthiswieneke.de>
2022-05-24 00:22:32 +0200

    convert multiple files at once

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { saveAs } from 'file-saver';
 import * as FileSaver from 'file-saver';
 
 @Component({
@@ -15,39 +14,44 @@ export class ConverterComponent {
   allowedImageTypes: Array<string> = ['jpg', 'jpeg', 'png'];
   widths: number[] = [ 2560, 1920, 1280 ];
   selectedWidth: number = 1920;
-  uploadedFile: File | undefined = undefined;
+  uploadedFiles: File[] | undefined = undefined;
   fileSelected = false;
   
   onFileSelected(event: any) {
-    this.uploadedFile = event.target.files[0];   
+    this.uploadedFiles = event.target.files;   
     this.fileSelected = true;   
   }
 
-  convertFile(){
-    if (this.uploadedFile && this.isValidImage(this.uploadedFile)) {
-      const formData = new FormData();
-      formData.append("thumbnail", this.uploadedFile);
+  convertFiles(){
+    if (this.uploadedFiles) {
+      for (const file of this.uploadedFiles) {
+        if(this.isValidImage(file)){
+          const formData = new FormData();
+          formData.append("thumbnail", file);
+    
+          const wembpImage = new Image();
+    
+          wembpImage.onload = () => {
+              // to keep the same aspect ratio as before
+              const ratio = wembpImage.naturalWidth / wembpImage.naturalHeight;
+    
+              const canvas = document.createElement('canvas');
+              canvas.width = this.selectedWidth;
+              canvas.height = this.selectedWidth / ratio;
+              canvas.getContext('2d')!.drawImage(wembpImage, 0, 0,canvas.width,canvas.height);
+              canvas.toBlob((blob) => {                
+                  if(blob)
+                    FileSaver.saveAs(blob, file.name + '.webp');
+              }, 'image/webp');
+              this.toastr.success(`${file.name} image was successfully converted to webp.`);
+          };
+          wembpImage.src = URL.createObjectURL(file);
 
-      const wembpImage = new Image();
-
-      wembpImage.onload = () => {
-          // to keep the same aspect ratio as before
-          const ratio = wembpImage.naturalWidth / wembpImage.naturalHeight;
-
-          const canvas = document.createElement('canvas');
-          canvas.width = this.selectedWidth;
-          canvas.height = this.selectedWidth / ratio;
-          canvas.getContext('2d')!.drawImage(wembpImage, 0, 0,canvas.width,canvas.height);
-          canvas.toBlob((blob) => {                
-              if(blob)
-                FileSaver.saveAs(blob, this.uploadedFile?.name + '.webp');
-          }, 'image/webp');
-      };
-
-      wembpImage.src = URL.createObjectURL(this.uploadedFile);
-  } else {
-    this.toastr.error(`Type not supported. Supported types are: ${this.allowedImageTypes.join(', ')}.`);
-  }
+        } else {
+          this.toastr.error(`Type not supported for ${file.name}. Supported types are: ${this.allowedImageTypes.join(', ')}.`);
+        }
+      }
+    } 
   }
 
   isValidImage(p_file:File): Boolean {
